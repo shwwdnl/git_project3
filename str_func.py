549667034041cca91f1e3a00c43abdf5d39dcc98
shwwdnl <shwwdnl@gmail.com>
2023-08-26 14:56:49 +0500

    add title_str update upper_str

--- str_func.py
+++ str_func.py
@@ -1,2 +1,6 @@
 def upper_str(string):
+    '''возвращает строку со всеми заглавными буквами'''
     return string.upper()
+def title_str(string):
+    '''возвращает строку с заглавной буквой, в каждом слове'''
+    return  string.title()
